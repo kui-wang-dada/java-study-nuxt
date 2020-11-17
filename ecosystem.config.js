2abfb02bafff3314be5f06501858e4f503c62ec9
kui wang <kuiwang@wholeren.com>
2020-11-17 21:41:46 +0800

    利用pm2自动化部署

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    apps: [
+        {
+            name: 'API',
+            script: './node_modules/nuxt/bin/nuxt.js',
+
+            // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+            args: 'one two',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'development'
+            },
+            env_production: {
+                NODE_ENV: 'production'
+            }
+        }
+    ],
+
+    deploy: {
+        production: {
+            user: 'root',
+            host: '121.196.189.114',
+            ref: 'origin/master',
+            repo: 'git@github.com:wangkui1991/java-study-nuxt.git',
+            path: '/root/wk/javastudy/production',
+            'post-deploy': 'npm install && npm run build &&  pm2 reload ecosystem.config.js --env production'
+        }
+    }
+};
